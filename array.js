96a6087fd4d1388038d76cb92584bdb26e0c0d05
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-07-30 02:52:41 +0600

    saw some example of array

---
+++ array.js
@@ -0,0 +1,31 @@
+// const -> value will not change 
+// let -> value will change 
+// var -> we should not use the var 
+
+// variable
+let amount = 200;
+
+// condition 
+if(amount > 100){
+    // here will be the statement 
+}
+else{
+    // and if the if condition fails to execute
+
+}
+
+// array 
+let number = [1, 2, 3, 4, 10, 99];
+console.log(number);
+
+for(let i of number){
+    console.log(i);
+}
+
+let fruits = ['apple', 'grapes', 'pineapple', 'sugercane', 'mangoes', 'guava'];
+console.log(fruits);
+
+for(let i of fruits){
+    console.log(i);
+    console.log('I want to eat');
+}
