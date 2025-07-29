9de490e95f6fb8ea7d63f483bcdff930148e609e
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-07-30 03:07:09 +0600

    while loop

---
+++ while.js
@@ -0,0 +1,20 @@
+let number = 7;
+
+if(number < 1){
+    console.log('Got a Small Number');
+}
+console.log('Something New is Cooking Here!');
+
+// While - loop 
+let num = 0;
+while(num < 5){
+    console.log('Successfully printing in while loop - ', num);
+    num++;
+    // num = num + 1
+    // num += 1
+}
+// Syntax of while loop 
+// while(condition){
+    // code execution, or the statement 
+    // increment or decrement 
+// }
