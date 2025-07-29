69d0cb46c9e25ac40652e657898043f99d16b22d
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-07-30 03:23:10 +0600

    some example of while loop

---
+++ range.js
@@ -0,0 +1,22 @@
+// let num = 1;
+// while(num  <= 10){
+//     console.log("Number : ", num)
+//     num++;
+// }
+
+let  num = 50;
+while(num <= 100){
+   if(num % 2 !== 0){
+    console.log(num);
+   }
+    num++;
+}
+
+let number = 1;
+let sum = 0;
+while(number <= 10){
+    sum += number;
+    console.log(number, "Sum : ", sum);
+    number++
+}
+console.log("Summiation of 1 to 10 is ",sum);
